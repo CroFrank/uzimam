53fbb3a4e00d8a3b69dadb3e57402b4f7878b76e
Fran <fducekuce@gmail.com>
2025-02-19 23:05:35 +0100

    again

--- src/middleware.ts
+++ src/middleware.ts
@@ -9,7 +9,7 @@ export const onRequest = defineMiddleware(async (context, next) => {
     context.locals.name = user?.user_metadata.name ?? null
     context.locals.email = user?.user_metadata.email ?? null
     context.locals.id = user?.id ?? null
-    console.log(user?.id)
+    console.log(`get from middelwarw ${user?.id}`)
   } catch (error) {
     console.error("Error fetching user:", error)
     context.locals.name = null
